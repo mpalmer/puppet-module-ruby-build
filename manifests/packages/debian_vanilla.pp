bc7a0f71a1a2299fb51635e27f1f0b7edce41a40
Matt Palmer <mpalmer@hezmatt.org>
2014-07-17 12:09:22 +1000

    Is that how ensure_packages works?

--- manifests/packages/debian_vanilla.pp
+++ manifests/packages/debian_vanilla.pp
@@ -7,24 +7,25 @@ class ruby_build::packages::debian_vanilla {
 	          before => Noop["ruby_build/packages/installed"]
 	        }
 	
-	ensure_packages("automake",
-	                "bison",
-	                "build-essential",
-	                "gawk",
-	                "git",
-	                "libffi-dev",
-	                "libgdbm-dev",
-	                "libncurses5-dev",
-	                "libncursesw5-dev",
-	                "libreadline6-dev",
-	                "libsqlite3-dev",
-	                "libssl-dev",
-	                "libxml2-dev",
-	                "libxslt1-dev",
-	                "libyaml-dev",
-	                "patch",
-	                "subversion",
-	                "zlib1g-dev",
+	ensure_packages(["automake",
+	                 "bison",
+	                 "build-essential",
+	                 "gawk",
+	                 "git",
+	                 "libffi-dev",
+	                 "libgdbm-dev",
+	                 "libncurses5-dev",
+	                 "libncursesw5-dev",
+	                 "libreadline6-dev",
+	                 "libsqlite3-dev",
+	                 "libssl-dev",
+	                 "libxml2-dev",
+	                 "libxslt1-dev",
+	                 "libyaml-dev",
+	                 "patch",
+	                 "subversion",
+	                 "zlib1g-dev"
+	                ],
 	                $opts
 	               )
 }
