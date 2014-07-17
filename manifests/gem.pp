0ef5113c8d8984cb6f53441fb8f2372892eb04e7
Matt Palmer <mpalmer@hezmatt.org>
2014-07-17 14:42:18 +1000

    srsly?

--- manifests/gem.pp
+++ manifests/gem.pp
@@ -53,7 +53,7 @@ define ruby_build::gem(
 
 	exec { "Install ${gem} into ruby_build ${definition} for ${name}":
 		command => "/usr/local/bin/chruby-exec $definition -- gem install $quoted_gem $version_opt $source_opt $docs_opt",
-		unless  => "/bin/test $(/usr/local/bin/chruby-exec $definition -- gem list --installed $quoted_gem $version_opt) = true",
+		unless  => "/usr/bin/test $(/usr/local/bin/chruby-exec $definition -- gem list --installed $quoted_gem $version_opt) = true",
 		require => Noop["ruby_build/definition/installed:${definition}"]
 	}
 }
